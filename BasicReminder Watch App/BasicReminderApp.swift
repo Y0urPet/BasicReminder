8693c9df87258a22f905aa192e41845c4cccdd05
timothy <tandrian24@bsd.idserve.net>
2024-05-26 19:25:42 +0700

    Initial Commit

---
+++ BasicReminder Watch App/BasicReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicReminderApp.swift
+//  BasicReminder Watch App
+//
+//  Created by Timothy Andrian on 26/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BasicReminder_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
